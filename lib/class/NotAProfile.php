eef3c592c50f68919497a67029174ca225e32621
simalgorta <simalgorta@ad8cd528-ad85-11de-a760-7b35dc761db7>
2009-11-09 03:38:59 +0000

    Implementé la función: darContactos.

--- lib/class/NotAProfile.php
+++ lib/class/NotAProfile.php
@@ -533,11 +533,26 @@ class NotAProfile{
 	 */
 	public static function darLlavesDisponiblesContactos(){
 		//TODO Hacer la consulta que devuelva las llaves disponibles de los contactos
+		
 		$sql = "SELECT * FROM llave WHERE reclamador_id IS NULL";
 		$llaves = DAO::doSQLAndReturn($sql);
 		return $llaves;
 	}
 	
+	/**
+	 * Este método retorna todos los contactos de un usuario dado.
+	 */
+	public static function darContactos(){
+		//Consulta que devuelve una tabla con la información de cada una de las llaves reclamadas al usuario logeado y la información del usuario reclamador.
+		$contactossql1 = sprintf("SELECT * FROM usuario LEFT JOIN llave ON usuario.id = llave.reclamador_id WHERE flag_aceptado = 1 AND creador_id = '%s'", $_SESSION['userid']);
+		$contactos1 = DAO::doSQLAndReturn($contactossql1);
+		//Consulta que devuelve una tabla con cada la información de cada una de las llaves reclamadas por usuario logeado y la información del usuario creador.
+		$contactossql2 = sprintf("SELECT * FROM usuario LEFT JOIN llave ON usuario.id = llave.creador_id WHERE flag_aceptado = 1 AND reclamador_id = '%s'", $_SESSION['userid']);
+		$contactos2 = DAO::doSQLAndReturn($contactossql2);
+		//Concatenamos ambos arreglos
+		$contactos = array_merge($contactos1, $contactos2);
+		return $contactos;
+	}
 	
 	
 //----------------------------------------------------------------------------------------------
