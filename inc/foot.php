2cf049db0c27abfa70d20a7113fc631ef2b3b8e4
mauricio.giraldo <mauricio.giraldo@ad8cd528-ad85-11de-a760-7b35dc761db7>
2009-11-21 22:21:38 +0000

    includes

---
+++ inc/foot.php
@@ -0,0 +1,3 @@
+	<div id="pie">
+		<a href="#">help</a> | <a href="#">about this project</a> | <a href="#">google code</a> | <a href="#">copyright notice</a>
+	</div>
---
+++ inc/head.php
@@ -0,0 +1,10 @@
+	<div id="cabezote">
+		<h1>not_a_profile</h1>
+		<ul id="nav">
+			<li><a href="#">keyring</a></li>
+			<li><a href="#">connections</a></li>
+		</ul>
+		<div id="userinfo">
+			rank: locksmith | <a href="#">log_out</a>
+		</div>
+	</div>
