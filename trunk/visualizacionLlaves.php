0c1959a6a4423b6a62f8852695ada0cb32f27b76
jguevara90 <jguevara90@ad8cd528-ad85-11de-a760-7b35dc761db7>
2009-11-24 03:34:51 +0000

    Corregido cabezote y pie

--- trunk/visualizacionLlaves.php
+++ trunk/visualizacionLlaves.php
@@ -103,16 +103,7 @@ $count = count($llaves);
 <body onload="initialize()" onunload="GUnload()">
 <div id="contenido">
 
-	<div id="cabezote">
-		<h1>not_a_profile</h1>
-		<ul id="nav">
-			<li><a href="#">keyring</a></li>
-			<li><a href="#">connections</a></li>
-		</ul>
-		<div id="userinfo">
-			rank: locksmith | <a href="#">log_out</a>
-		</div>
-	</div>
+	<?php include("./inc/cabezote.php"); ?>
 	
 	<div id="cuerpo">
 		<div id="llaves">
@@ -169,9 +160,7 @@ $count = count($llaves);
 		</div>
 	</div>
 	
-	<div id="pie">
-		<a href="#">help</a> | <a href="#">about this project</a> | <a href="#">google code</a> | <a href="#">copyright notice</a>
-	</div>
+	<?php include("./inc/pie.php"); ?>
 
 </div>
 </body>
