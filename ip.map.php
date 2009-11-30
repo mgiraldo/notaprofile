a517140059d1d8d3718662e559c2668a83b9e03a
mauricio.giraldo <mauricio.giraldo@ad8cd528-ad85-11de-a760-7b35dc761db7>
2009-11-30 09:16:45 +0000

    latitud/longitud se pifiaba en insertar llave

--- ip.map.php
+++ ip.map.php
@@ -13,6 +13,7 @@ if(!NotAProfile::estaLogeado()) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>not_a_profile : gps key</title>
+<link href="/css/estilos.css" rel="stylesheet" type="text/css" />
 <meta content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" name="viewport"/>
 <style type="text/css">
 <!--
@@ -40,12 +41,13 @@ body {
 <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=true&amp;key=<?php echo $app["apiKey"] ?>" type="text/javascript"></script>
 <script>
 var map;
+var marker;
 function initialize() {
 	if (GBrowserIsCompatible()) {
 		map = new GMap2(document.getElementById("gmap"));
 		map.setCenter(new GLatLng(4.620913,-74.083643, true), 13);
 		map.setUIToDefault();
-		invokeLocation();
+		invokeLocation();	
 	}
 }
 function invokeLocation() {
@@ -54,21 +56,29 @@ function invokeLocation() {
 function gMapper (position) {
 	var myLatLng = new GLatLng(position.coords.latitude, position.coords.longitude, true);
 	map.setCenter(myLatLng, 18);
-    var marker = new GMarker(myLatLng, {draggable: true, clickable: true, bouncy: false});
+    marker = new GMarker(myLatLng, {draggable: true, clickable: true, bouncy: false});
 
+	GEvent.addListener(map, "click", function(overlay,pos) {
+		if (overlay==null && pos) {
+			marker.closeInfoWindow();
+			marker.setLatLng(pos);
+			marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+pos.lat()+","+pos.lng()+"&body=write_here_or_delete_this\">create a key here</a></div>", {maxWidth:225});
+		}
+	});
+	
     GEvent.addListener(marker, "dragstart", function() {
-    	map.closeInfoWindow();
+    	marker.closeInfoWindow();
     });
 
     GEvent.addListener(marker, "dragend", function(pos) {
 							if (pos) {
-    							marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+pos.lat()+","+pos.lng()+"&body=write_here_or_delete_this\">create a key here</a><br />or drag the pointer to your location</div>", {maxWidth:225});
+    							marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+pos.lat()+","+pos.lng()+"&body=write_here_or_delete_this\">create a key here</a></div>", {maxWidth:225});
 							}
     });
 
     GEvent.addListener(marker, "click", function(pos) {
 							if (pos) {
-    							marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+pos.lat()+","+pos.lng()+"&body=write_here_or_delete_this\">create a key here</a><br />or drag the pointer to your location</div>", {maxWidth:225});
+    							marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+pos.lat()+","+pos.lng()+"&body=write_here_or_delete_this\">create a key here</a></div>", {maxWidth:225});
 							}
     });
 
