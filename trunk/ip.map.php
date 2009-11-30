d15bf15b220a51e9f577b9750fc4c30377b3e14e
mauricio.giraldo <mauricio.giraldo@ad8cd528-ad85-11de-a760-7b35dc761db7>
2009-11-30 05:57:49 +0000

    ya funciona el gps en notaprofile.com/gps y otros cambios

--- trunk/ip.map.php
+++ trunk/ip.map.php
@@ -1,24 +1,92 @@
-<?
+<?php
+
 require_once 'lib/class/NotAProfile.php';
 global $app;
-xml version="1.0" encoding="UTF-8"?>
+
+?>
+<?php echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" ?>
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>iPhone 3.0 geolocation demo</title>
+<title>not_a_profile : gps key</title>
 <meta content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" name="viewport"/>
-<script>
-function handler(location) {
-var message = document.getElementById("message");
-message.innerHTML ="<img src='http://maps.google.com/staticmap?center=" + location.coords.latitude + "," + location.coords.longitude + "&size=300x200&maptype=hybrid&key=<?php echo($app['apiKey']);?> />";
-message.innerHTML+="<p>Longitude: " + location.coords.longitude + "</p>";
-message.innerHTML+="<p>Accuracy: " + location.coords.accuracy + "</p>";
-message.innerHTML+="<p>Latitude: " + location.coords.latitude + "</p>";
+<style type="text/css">
+<!--
+* {
+	margin: 0;
+	padding: 0;
 }
-navigator.geolocation.getCurrentPosition(handler);
+body,td,th {
+	font-family: Courier New, Courier, monospace;
+	font-size: 16px;
+	color: #FFF;
+}
+body {
+	background-color: #000;
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+#gmap {
+	height: 416px;
+}
+#bubble {
+	color: #000;
+}
+-->
+</style>
+<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=true&amp;key=<?php echo $app["apiKey"] ?>" type="text/javascript"></script>
+<script>
+var map;
+function initialize() {
+	if (GBrowserIsCompatible()) {
+		map = new GMap2(document.getElementById("gmap"));
+		map.setCenter(new GLatLng(4.620913,-74.083643, true), 13);
+		map.setUIToDefault();
+		invokeLocation();
+	}
+}
+function invokeLocation() {
+	navigator.geolocation.getCurrentPosition(gMapper);
+}
+function gMapper (position) {
+	var myLatLng = new GLatLng(position.coords.latitude, position.coords.longitude, true);
+	map.setCenter(myLatLng, 18);
+    var marker = new GMarker(myLatLng, {draggable: true, clickable: true, bouncy: false});
+
+    GEvent.addListener(marker, "dragstart", function() {
+    	map.closeInfoWindow();
+    });
+
+    GEvent.addListener(marker, "dragend", function(pos) {
+							if (pos) {
+    							marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+pos.lat()+","+pos.lng()+"&body=write_here_or_delete_this\">create a key here</a><br />or drag the pointer to your location</div>", {maxWidth:225});
+							}
+    });
+
+    GEvent.addListener(marker, "click", function(pos) {
+							if (pos) {
+    							marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+pos.lat()+","+pos.lng()+"&body=write_here_or_delete_this\">create a key here</a><br />or drag the pointer to your location</div>", {maxWidth:225});
+							}
+    });
+
+    marker.openInfoWindowHtml("<div id=\"bubble\"><a href=\"mailto:upload@notaprofile.com?subject="+position.coords.latitude+","+position.coords.longitude+"&body=write_here_or_delete_this\">create a key here</a><br />or drag the pointer to your location</div>", {maxWidth:225});
+
+	map.addOverlay(marker);
+}
+function handler(location) {
+	var message = document.getElementById("gmap");
+	message.innerHTML ="<img src='http://maps.google.com/staticmap?center=" + location.coords.latitude + "," + location.coords.longitude + "&size=300x300&maptype=hybrid&zoom=16&key=<?php echo $app["apiKey"] ?>' />";
+	// message.innerHTML+="<p>Longitude: " + location.coords.longitude + "</p>";
+	// message.innerHTML+="<p>Latitude: " + location.coords.latitude + "</p>";
+	// message.innerHTML+="<p>Accuracy: " + location.coords.accuracy + "</p>";
+	message.innerHTML+="<p><a href=\"mailto:upload@notaprofile.com?subject="+location.coords.latitude+","+location.coords.longitude+"&body=write_here_or_delete_this\"><img src=\"/img/btn_create.gif\" /></a></p>";
+}
 </script>
-</head>
-<body>
-<div id="message">Location unknown</div>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head>
+<body onload="initialize()" onunload="GUnload()">
+<div id="gmap">Location unknown</div>
+<!-- <div id="button"><a href="#"><img src="img/submit.png" width="195" height="26" alt="submit_key" /></a></div> -->
 </body>
 </html>
